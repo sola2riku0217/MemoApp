698c2a948845f51e0b69dd2927db6d06d38693f7
sola <sola2riku0217@gmail.com>
2021-11-15 23:11:52 +0900

    change Icon

--- src/components/memoList.jsx
+++ src/components/memoList.jsx
@@ -1,6 +1,7 @@
-import { Feather } from '@expo/vector-icons';
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+
+import Icon from './Icon';
 
 export default function MemoList() {
   return (
@@ -12,7 +13,7 @@ export default function MemoList() {
           <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
         </View>
         <View>
-          <Feather name="x" size={16} color="#B0B0B0" />
+          <Icon name="delete" size={24} color="#B0B0B0" />
         </View>
       </View>
       {/* memo List② */}
@@ -22,7 +23,7 @@ export default function MemoList() {
           <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
         </View>
         <View>
-          <Feather name="x" size={16} color="#B0B0B0" />
+          <Icon name="delete" size={24} color="#B0B0B0" />
         </View>
       </View>
       {/* memo List③ */}
@@ -32,7 +33,7 @@ export default function MemoList() {
           <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
         </View>
         <View>
-          <Feather name="x" size={16} color="#B0B0B0" />
+          <Icon name="delete" size={24} color="#B0B0B0" />
         </View>
       </View>
     </View>
