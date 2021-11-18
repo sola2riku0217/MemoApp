0b7990f7ff33492341fec4e1486ebaf7bca3d933
sola <sola2riku0217@gmail.com>
2021-11-18 22:54:46 +0900

    create Login SignUp Screen

---
+++ src/screens/LogInScreen.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+  View, Text, StyleSheet, TextInput,
+} from 'react-native';
+
+import Appbar from '../components/appbar';
+import Button from '../components/Button';
+
+export default function LogInScreen() {
+  return (
+    <View style={styles.container}>
+      <Appbar />
+      <View style={styles.inner}>
+        <Text style={styles.title}>Log In</Text>
+        <TextInput style={styles.input} value="Email Address" />
+        <TextInput style={styles.input} value="password" />
+        <Button label="submit" />
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Not registered?</Text>
+          <Text style={styles.footerLink}>Sign up here!!</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F4F8',
+  },
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 24,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  input: {
+    fontSize: 16,
+    height: 48,
+    borderColor: '#DDDDDD',
+    borderWidth: 1,
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#467fd3',
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+});
--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  View, TextInput, StyleSheet, KeyboardAvoidingView,
+  View, TextInput, StyleSheet,
 } from 'react-native';
 
 import Appbar from '../components/appbar';
